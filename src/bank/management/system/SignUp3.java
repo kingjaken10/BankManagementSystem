f6e5eeb885d12c71f36388b811640f79cade7e69
kingjaken10 <megan.nv.ngo@gmail.com>
2025-07-31 16:44:53 -0500

    Initial commit

---
+++ src/bank/management/system/SignUp3.java
@@ -0,0 +1,16 @@
+package bank.management.system;
+
+import java.awt.*;
+
+import javax.swing.*;
+ 
+public class SignUp3 extends JFrame{
+
+    SignUp3(){
+
+    }
+    
+    public static void main(String[] args){
+        new SignUp3();
+    }
+}
