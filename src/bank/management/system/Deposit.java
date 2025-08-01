967da311ce86a0759c93c4db281674dbfe09ab45
kingjaken10 <megan.nv.ngo@gmail.com>
2025-08-01 13:52:19 -0500

    Initial commit

---
+++ src/bank/management/system/Deposit.java
@@ -0,0 +1,16 @@
+package bank.management.system;
+
+import java.awt.*;
+
+import javax.swing.*;
+
+public class Deposit extends JFrame{
+
+    Deposit(){
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
