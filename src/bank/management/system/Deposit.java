1573842bae746a8d5a369614c830a55ede703641
kingjaken10 <megan.nv.ngo@gmail.com>
2025-08-01 15:53:51 -0500

    Added all components to window

--- src/bank/management/system/Deposit.java
+++ src/bank/management/system/Deposit.java
@@ -5,12 +5,70 @@ import java.awt.*;
 import javax.swing.*;
 
 public class Deposit extends JFrame{
+    
+    // components to be placed on window
+    JTextField depositAmtText;   // deposit amount text field
+    JButton depositButton, backButton;  // deposit and back buttons 
 
-    Deposit(){
+    String  pin;    // stores pin number
 
+    Deposit(String pin){
+        super("Deposit");   // call parent constructor 
+
+        this.pin = pin; // store pin
+
+        // set up window
+        setLayout(null);
+        setSize(1545, 1000);  // set window size
+        setLocationRelativeTo(null);    // center align window
+        setResizable(false); // fix size of window
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // set default close operation
+
+        // set background image of window (ATM image)
+        ImageIcon atm1 =  new ImageIcon(ClassLoader.getSystemResource("icons/atm2.png"));   // load the image
+        Image atm2 = atm1.getImage().getScaledInstance(1545, 830, Image.SCALE_DEFAULT);    // resize the image
+        ImageIcon atm3 = new ImageIcon(atm2);   // create a new ImageIcon to store the resized image
+        JLabel atmImage = new JLabel(atm3); // add image to a label
+        atmImage.setBounds(0, 0, 1545, 830);   // resize and position the label
+        add(atmImage);  // add image to window
+
+        // add labels and respective components to window
+        // add deposit amount label
+        JLabel depositAmtLabel = new JLabel("ENTER DEPOSIT AMOUNT");    // create deposit amount label
+        depositAmtLabel.setFont(new Font("System", Font.BOLD, 16)); // set font
+        depositAmtLabel.setBounds(520, 170, 400, 35);   // set bounds
+        depositAmtLabel.setForeground(Color.WHITE); // set text color to white
+        atmImage.add(depositAmtLabel);  // add deposit amount label on ATM image
+
+        // add deposit amount text field
+        depositAmtText = new JTextField();   // create deposit amount text field
+        depositAmtText.setFont(new Font("System", Font.BOLD, 22)); // set font
+        depositAmtText.setBounds(465, 210, 320, 30);    // set bounds
+        depositAmtText.setForeground(Color.WHITE);  // set text color to white
+        depositAmtText.setBackground(new Color(65, 125, 128));  // set background color
+        depositAmtText.setHorizontalAlignment(JTextField.CENTER);   // center align text
+        depositAmtText.setCaretColor(Color.WHITE); // make cursor white
+        atmImage.add(depositAmtText);   // add deposit amount text field on ATM image
+
+        // add deposit button
+        depositButton = new JButton("DEPOSIT"); // create deposit button
+        depositButton.setBounds(710, 364, 130, 34);    // set bounds
+        depositButton.setForeground(Color.WHITE);  // set text color to white
+        depositButton.setBackground(new Color(65, 125, 128));  // set background color
+        atmImage.add(depositButton);   // add deposit button on ATM image
+
+        // add back button
+        backButton = new JButton("BACK"); // create back button
+        backButton.setBounds(710, 410, 130, 34);    // set bounds
+        backButton.setForeground(Color.WHITE);  // set text color to white
+        backButton.setBackground(new Color(65, 125, 128));  // set background color
+        atmImage.add(backButton);   // add back button on ATM image
+
+
+        setVisible(true);  // make window visible
     }
 
     public static void main(String[] args) {
-        
+        new Deposit("null");
     }
 }
