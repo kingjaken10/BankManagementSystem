2ca39226c8c3814c61ce28d1261d2a91af58dd9e
kingjaken10 <megan.nv.ngo@gmail.com>
2025-08-02 14:53:37 -0500

    Initial commit

---
+++ src/bank/management/system/Withdrawal.java
@@ -0,0 +1,16 @@
+package bank.management.system;
+
+import java.awt.*;
+
+import javax.swing.*;
+
+public class Withdrawal extends JFrame{
+
+    Withdrawal(){
+
+    }
+
+    public static void main(String[] args) {
+        new Withdrawal();
+    }
+}
