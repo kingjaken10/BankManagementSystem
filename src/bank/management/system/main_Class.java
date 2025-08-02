7ae24b0c289f9c7299e537ee2fc0480061e6a0e1
kingjaken10 <megan.nv.ngo@gmail.com>
2025-08-02 00:05:00 -0500

    Initial commit

---
+++ src/bank/management/system/main_Class.java
@@ -0,0 +1,16 @@
+package bank.management.system;
+
+import java.awt.*;
+
+import javax.swing.*;
+
+public class main_Class extends JFrame{
+
+    main_Class(){
+        
+    }
+
+    public static void main(String[] args) {
+        new main_Class();
+    }
+}
