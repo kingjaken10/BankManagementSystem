73e3c4844b7d3be6c7c62a79ad12a058d299c227
kingjaken10 <megan.nv.ngo@gmail.com>
2025-08-02 00:14:18 -0500

    Set up window

--- src/bank/management/system/main_Class.java
+++ src/bank/management/system/main_Class.java
@@ -7,7 +7,24 @@ import javax.swing.*;
 public class main_Class extends JFrame{
 
     main_Class(){
+        super("Transaction");   // call parent constructor 
         
+        // set up window
+        setLayout(null);
+        setSize(1545, 1000);  // set window size
+        setLocationRelativeTo(null);    // center align window
+        setResizable(false); // fix size of window
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // set default close operation
+
+        // set background image of window (ATM image)
+        ImageIcon atm1 =  new ImageIcon(ClassLoader.getSystemResource("icons/atm2.png"));   // load the image
+        Image atm2 = atm1.getImage().getScaledInstance(1545, 830, Image.SCALE_DEFAULT);    // resize the image
+        ImageIcon atm3 = new ImageIcon(atm2);   // create a new ImageIcon to store the resized image
+        JLabel atmImage = new JLabel(atm3); // add image to a label
+        atmImage.setBounds(0, 0, 1545, 830);   // resize and position the label
+        add(atmImage);  // add image to window
+
+        setVisible(true);   // make window visible
     }
 
     public static void main(String[] args) {
