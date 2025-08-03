d02450f2040f4329087c25e2eb0142697a382fb8
kingjaken10 <megan.nv.ngo@gmail.com>
2025-08-02 23:47:38 -0500

    Initial commit

---
+++ src/bank/management/system/FastCash.java
@@ -0,0 +1,16 @@
+package bank.management.system;
+
+import java.awt.*;
+
+import javax.swing.*;
+
+public class FastCash extends JFrame{
+    
+    FastCash(){
+
+    }
+
+    public static void main(String[] args) {
+        new FastCash();
+    }
+}
