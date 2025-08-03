8982c8d9fc97abc0bc4a86ce7ec69a596765f259
kingjaken10 <megan.nv.ngo@gmail.com>
2025-08-03 14:05:21 -0500

    Initial commit

---
+++ src/bank/management/system/Pin.java
@@ -0,0 +1,16 @@
+package bank.management.system;
+
+import java.awt.*;
+
+import javax.swing.*;
+
+public class Pin extends JFrame{
+    
+    Pin(){
+
+    }
+
+    public static void main(String[] args) {
+        new Pin();
+    }
+}
