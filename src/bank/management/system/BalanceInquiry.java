97acb7c357ae1fab610b86f43e9fecc10769ba34
kingjaken10 <megan.nv.ngo@gmail.com>
2025-08-02 22:48:54 -0500

    Initial commit

---
+++ src/bank/management/system/BalanceInquiry.java
@@ -0,0 +1,16 @@
+package bank.management.system;
+
+import java.awt.*;
+
+import javax.swing.*;
+
+public class BalanceInquiry extends JFrame{
+
+    BalanceInquiry(){
+
+    }
+
+    public static void main(String[] args) {
+        new BalanceInquiry();
+    }
+}
