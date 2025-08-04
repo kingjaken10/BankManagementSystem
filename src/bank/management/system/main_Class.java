71802cec7c78c14792739e611c100db04cb89611
kingjaken10 <megan.nv.ngo@gmail.com>
2025-08-04 15:41:52 -0500

    Edited window and added function of enter key

--- src/bank/management/system/main_Class.java
+++ src/bank/management/system/main_Class.java
@@ -22,15 +22,16 @@ public class main_Class extends JFrame implements ActionListener{
         setLayout(null);
         setSize(1545, 1000);  // set window size
         setLocationRelativeTo(null);    // center align window
+        setUndecorated(true);   // hides window border
         setResizable(false); // fix size of window
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // set default close operation
 
         // set background image of window (ATM image)
         ImageIcon atm1 =  new ImageIcon(ClassLoader.getSystemResource("icons/atm2.png"));   // load the image
-        Image atm2 = atm1.getImage().getScaledInstance(1545, 835, Image.SCALE_DEFAULT);    // resize the image
+        Image atm2 = atm1.getImage().getScaledInstance(1545, 870, Image.SCALE_DEFAULT);    // resize the image
         ImageIcon atm3 = new ImageIcon(atm2);   // create a new ImageIcon to store the resized image
         JLabel atmImage = new JLabel(atm3); // add image to a label
-        atmImage.setBounds(0, 0, 1545, 835);   // resize and position the label
+        atmImage.setBounds(0, 0, 1545, 870);   // resize and position the label
         add(atmImage);  // add image to window
 
         // add labels and buttons to window
