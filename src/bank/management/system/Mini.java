1a9629dc64535f749cea9f3e4b708f0d359c43c6
kingjaken10 <megan.nv.ngo@gmail.com>
2025-08-03 20:31:28 -0500

    Initial commit

---
+++ src/bank/management/system/Mini.java
@@ -0,0 +1,16 @@
+package bank.management.system;
+
+import java.awt.*;
+
+import javax.swing.*;
+
+public class Mini extends JFrame{
+
+    Mini(){
+
+    }
+    
+    public static void main(String[] args) {
+        new Mini();
+    }
+}
